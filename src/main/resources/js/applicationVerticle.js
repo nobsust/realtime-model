4d44c631f09e9ca7f16d7e232e8b8d48ea00ee43
Sam <nobsust@gmail.com>
2017-02-11 11:39:51 +0530

    pack() added -  id calculation

--- src/main/resources/js/applicationVerticle.js
+++ src/main/resources/js/applicationVerticle.js
@@ -20,7 +20,37 @@ var field = {};
 claz.fields.push(field);
 field.id = 'firstName';
 field.name = 'First Name';
+field.type = 'text';
 
+function pack(app) {
+
+    var id = app.id;
+    // modules
+    for(var m = 0; m < app.modules.length; m++) {
+        var module = app.modules[m];
+        id += '.' + module.id;
+        // set module id
+        module.id = id;
+
+        // classes
+        for(var c = 0; c < module.classes.length; c++) {
+            var claz = module.classes[c];
+            id += '.' + claz.id;
+            // set claz id
+            claz.id = id;
+
+            for(var f = 0; f < claz.fields.length; f++) {
+                var field = claz.fields[f];
+                id += '.' + field.id;
+                // set field id
+                field.id = id;
+            }
+        }
+    }
+
+}
+
+var packedApp = pack(app);
 
 vertx.setPeriodic(3000, function(id) {
     vertx.eventBus().publish("application-feed", app);
